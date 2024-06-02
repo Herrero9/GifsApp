74e2fd390db69921960e4f671ecba3aa6297a3af
Sergio Herrero <serghebo@hotmail.es>
2024-06-02 21:02:21 +0200

    home page created

---
+++ src/app/gifs/pages/home/home.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',  
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  
+  constructor() {}
+  
+ }
